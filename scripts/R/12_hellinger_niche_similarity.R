846e997c9a069ea9cc218ac5ee52bde2da026114
mlrog <80300034+mlrog@users.noreply.github.com>
2026-04-23 08:06:39 -0700

    Update 12_hellinger_niche_similarity.R

--- scripts/R/12_hellinger_niche_similarity.R
+++ scripts/R/12_hellinger_niche_similarity.R
@@ -31,7 +31,13 @@
 #
 # Outputs: - hellinger_within_species_2D_vs_3D.csv
 #
-# Paper:   [Full citation or "In review, Journal of Urban Ecosystems"]
+# Paper:   Rogers, M. L., Frazier, A. E., Zellmer, A. J., & 
+#          Lerman, S. B. (2026). Sensitivity of habitat 
+#          suitability-derived connectivity models to 
+#          three-dimensional measures of urban landscape structure. 
+#          Urban Ecosystems, 29(3), 121. 
+#          https://doi.org/10.1007/s11252-026-01948-y
+
 # =============================================================
 
 
