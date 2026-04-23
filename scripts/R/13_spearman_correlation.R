52fa264df6a1add6890d5bfc7e418c4ec4c27012
mlrog <80300034+mlrog@users.noreply.github.com>
2026-04-23 08:06:57 -0700

    Update 13_spearman_correlation.R

--- scripts/R/13_spearman_correlation.R
+++ scripts/R/13_spearman_correlation.R
@@ -12,7 +12,13 @@
 #
 # Outputs: - spearman_correlation_all_species.csv
 #
-# Paper:   [Full citation or "In review, Journal of Urban Ecosystems"]
+# Paper:   Rogers, M. L., Frazier, A. E., Zellmer, A. J., & 
+#          Lerman, S. B. (2026). Sensitivity of habitat 
+#          suitability-derived connectivity models to 
+#          three-dimensional measures of urban landscape structure. 
+#          Urban Ecosystems, 29(3), 121. 
+#          https://doi.org/10.1007/s11252-026-01948-y
+
 # =============================================================
 
 
